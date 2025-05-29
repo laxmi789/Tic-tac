ae3c67e21e9be1f962222ed87921e1e612f277e6
laxmi789 <93903213+laxmi789@users.noreply.github.com>
2025-05-29 14:40:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1);
   }
 
- /* function jumpTo(nextMove) {
+  function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
@@ -81,16 +81,16 @@ export default function Game() {
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
-  });*/
+  });
 
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      {/* <div className="game-info">
+       <div className="game-info">
         <ol>{moves}</ol>
-      </div> */}
+      </div> 
     </div>
   );
 }
@@ -109,237 +109,11 @@ function calculateWinner(sq) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    //console.log(a, b, c);
+    
     if (sq[a] && sq[a] === sq[b] && sq[a] === sq[c]) {      
           return sq[a];
     }
   }
   return null;
 }
-
-/*function Square(){
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-   // console.log('clicked');
-  }
-  return (
-    <button className="square" onClick={handleClick}>{value}</button>
-  );
-  
-}
-
-export default function Board(){
-  return (
-    <Square />
-  );
-}
-
-
-/*function Category({category})
-{
-  return(
-    <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
-  );
-
-}
-function Product({products}){
-  const name = products.stocked ? products.name:<span style={{color:"red"}}>{products.name}</span>;
-return(
-  //products.foreach(){
-  <tr>   
-    <td>{name}</td>
-    <td>{products.price}</td>
-  </tr>
-  //}
-);
-}
-function ProductList({products}){
-const row = [];
-let lastCategory = null;
-
-products.forEach((product) => {
-  if (product.category !== lastCategory) {
-    row.push(
-      <Category category={product.category} key={product.category} />
-    );
-}
-row.push(
-  <Product products={product} key={product.name} />
-);
-
-lastCategory = product.category;
-});
-
-return (
-  <table>
-    <thead>
-    <tr>
-      <th>Name</th>
-      <th>Price</th>
-    </tr>
-    </thead>
-    <tbody>{row}</tbody>
-  </table>
-);
-}
-
-function Search(){
-  return(
-    <form>
-    <input type="text" name="search" placeholder="search for..." />
-    <label>
-    <input type="checkbox" name="search" /> only show product in stock
-    </label>
-    </form>
-  );
-}
-function Productinfo({products}){
-  return (
-    <div> 
-      <Search />     
-      <ProductList products={products} />
-    </div>
-  );
-}
-  const products = [{category:"fruits", stocked: true, name: "apple", price: "25" },{category:"vegetable", stocked: false, name: "pinapple", price: "30 "}];
-
-
-export default function Display(){
-  return(
- <Productinfo products={products} />
-  );
-}*/
-
-/*import { useState } from 'react';
-
-function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = useState('');
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  return (
-    <div>
-      <SearchBar 
-        filterText={filterText} 
-        inStockOnly={inStockOnly} 
-        onFilterTextChange={setFilterText} 
-        onInStockOnlyChange={setInStockOnly} />
-        
-      <ProductTable 
-        products={products} 
-        filterText={filterText}
-        inStockOnly={inStockOnly} />
-    </div>
-  );
-}
-
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? product.name :
-    <span style={{ color: 'red' }}>
-      {product.name}
-    </span>;
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products, filterText, inStockOnly }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (
-      product.name.toLowerCase().indexOf(
-        filterText.toLowerCase()
-      ) === -1
-    ) {
-      return;
-    }
-    if (inStockOnly && !product.stocked) {
-      return;
-    }
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
-    );
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar({
-  filterText,
-  inStockOnly,
-  onFilterTextChange,
-  onInStockOnlyChange
-}) {
-  return (
-    <form>
-      <input 
-        type="text" 
-        value={filterText} placeholder="Search..." 
-        onChange={(e) => onFilterTextChange(e.target.value)} />
-      <label>
-        <input 
-          type="checkbox" 
-          checked={inStockOnly} 
-          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
-}
-*/
 
